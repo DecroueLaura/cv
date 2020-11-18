2d92192b7b02e44e1a91eba0c84fea1638477d9a
Decrouelaura <decrouelaura@gmail.com>
2020-11-18 13:21:14 +0100

    premier commit

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("nav").style.width = "10%";
+  document.getElementById("body").style.marginLeft = "10%";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("nav").style.width = "0";
+  document.getElementById("body").style.marginLeft = "0";
+}
