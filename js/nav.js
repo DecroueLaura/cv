494d5e90c68360b67d340e344aa395a41ff5a16f
Decrouelaura <decrouelaura@gmail.com>
2020-11-18 17:01:17 +0100

    modif du cv

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,13 @@
 function openNav() {
-  document.getElementById("nav").style.width = "10%";
-  document.getElementById("body").style.marginLeft = "10%";
+  document.getElementById("nav").style.width = "12%";
+  document.getElementById("body").style.marginLeft = "12%";
+  document.getElementById("open").style.display = "none";
+
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("nav").style.width = "0";
   document.getElementById("body").style.marginLeft = "0";
+  document.getElementById("open").style.display = "block";
 }
