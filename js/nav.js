be38e939a904220aff24df4698782ceb6f28df0a
Decrouelaura <decrouelaura@gmail.com>
2020-12-01 11:47:18 +0100

    modification portfolio*

--- js/nav.js
+++ js/nav.js
@@ -12,15 +12,15 @@ function closeNav() {
   document.getElementById("open").style.display = "block";
 }
 
-function openCity(event, cityName) {
-  // Declare all variables
-  var i, tabcontent, subcontent, sublinks, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+// function openCity(event, cityName) {
+//   // Declare all variables
+//   var i, tabcontent, subcontent, sublinks, tablinks;
+//
+//   // Get all elements with class="tabcontent" and hide them
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
 
   // subcontent = document.getElementsByClassName("subcontent");
   // console.log(subcontent);
@@ -28,22 +28,22 @@ function openCity(event, cityName) {
   //   subcontent[i].style.display = "none";
   // }
 
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Get all elements with class="sublinks" and remove the class "active"
-  sublinks = document.getElementsByClassName("sublinks");
-  for (i = 0; i < sublinks.length; i++) {
-    sublinks[i].className = sublinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
-  event.currentTarget.className += " active";
-}
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+//   // Get all elements with class="tablinks" and remove the class "active"
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+//
+//   // Get all elements with class="sublinks" and remove the class "active"
+//   sublinks = document.getElementsByClassName("sublinks");
+//   for (i = 0; i < sublinks.length; i++) {
+//     sublinks[i].className = sublinks[i].className.replace(" active", "");
+//   }
+//
+//   // Show the current tab, and add an "active" class to the button that opened the tab
+//   document.getElementById(cityName).style.display = "block";
+//   event.currentTarget.className += " active";
+// }
+//
+// // Get the element with id="defaultOpen" and click on it
+// document.getElementById("defaultOpen").click();
